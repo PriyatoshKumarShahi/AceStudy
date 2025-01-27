c7b621297692b38a13ee716923a3c0493a44ce26
ayushi-shahi <ayushishaho14072004@gmail.com>
2025-01-27 14:39:10 +0530

    Changing css and adding one more section

--- js/category.js
+++ js/category.js
@@ -51,3 +51,19 @@ exploreButtons.forEach((button) => {
     window.location.href = `/${category}-page.html`;
   });
 });
+const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo a');
+const hamburgerSpan = document.querySelector('.hamburger span');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+    logo.classList.add('logo-scrolled');
+    hamburger.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+    logo.classList.remove('logo-scrolled');
+    hamburger.classList.remove('scrolled');
+
+  }
+});
